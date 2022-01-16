af865c8fae0bb260119dc2a0584ffb2a82cf1aee
Manoj Sara <manojsara@Manojs-MacBook-Pro.local>
2022-01-16 15:40:19 -0800

    edited bfs dfs

--- pacai/student/search.py
+++ pacai/student/search.py
@@ -2,6 +2,11 @@
 In this file, you will implement generic search algorithms which are called by Pacman agents.
 """
 
+from pacai.util.stack import Stack
+from pacai.util.queue import Queue
+from pacai.util.priorityQueue import PriorityQueue
+
+
 def depthFirstSearch(problem):
     """
     Search the deepest nodes in the search tree first [p 85].
@@ -19,7 +24,44 @@ def depthFirstSearch(problem):
     """
 
     # *** Your Code Here ***
-    raise NotImplementedError()
+    print("Start: %s" % (str(problem.startingState())))
+    print("Is the start a goal?: %s" % (problem.isGoal(problem.startingState())))
+    print("Start's successors: %s" % (problem.successorStates(problem.startingState())))
+
+    if problem.isGoal(problem.startingState()):
+         return "DONE"
+    fringe = Stack()
+    fringe.push((problem.startingState(), 'STOP', 0))
+    reached = {problem.startingState()}
+    while fringe.isEmpty() is False:
+        node = fringe.pop()
+        for child in problem.successorStates(node[0]):
+            if problem.isGoal(child[0]):
+                fringe.push(child)
+                break
+            if child[0] not in reached:
+                reached.add(child[0])
+                fringe.push(child)
+    directions = []
+    while fringe.isEmpty() is False:
+        directions.append((fringe.pop())[1])
+    print(directions)
+    return directions
+
+def dfsRecur(problem, stack, check):
+    for s in problem.successorStates(stack[-1][0]):
+        if s[0] in check:
+            continue
+        stack.push(s)
+        check.add(s[0])
+        if problem.isGoal(s[0]):
+            return stack
+        dfsRecur(problem, stack, check)
+        if problem.isGoal(stack[-1][0]):
+            return stack
+        stack.pop()
+    return stack
+
 
 def breadthFirstSearch(problem):
     """
@@ -27,7 +69,22 @@ def breadthFirstSearch(problem):
     """
 
     # *** Your Code Here ***
-    raise NotImplementedError()
+    if problem.isGoal(problem.startingState()):
+        return "DONE"
+    fringe = Queue()
+    fringe.push((problem.startingState(), 'STOP', 0))
+    reached = {problem.startingState()}
+    while fringe.isEmpty() is False:
+        node = fringe.pop()
+        for child in problem.successorStates(node[0]):
+            if problem.isGoal(child[0]):
+                fringe.push(child)
+                break
+            if child[0] not in reached:
+                reached.add(child[0])
+                fringe.push(child)
+    while fringe.isEmpty() is False:
+        print(fringe.pop())
 
 def uniformCostSearch(problem):
     """
